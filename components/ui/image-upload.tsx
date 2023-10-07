a6385c4bb226ea9e4b84d12158cf4939c4934f41
se64vet <mv.thienphu@gmail.com>
2023-10-07 19:46:07 -0400

    Adding product page UI and API endpoints

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,94 +1,80 @@
-'use client'
+"use client";
 
-import { CldUploadWidget } from "next-cloudinary";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import { PlusCircle, Trash } from "lucide-react";
+import { CldUploadWidget } from 'next-cloudinary';
+import { useEffect, useState } from 'react';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import { ImagePlus, Trash } from 'lucide-react';
 
 interface ImageUploadProps {
-    values : string[] | [],
-    onChange: (imgUrl:string) => void, 
-    onRemove: (emptyString:string) => void,
-    loading: boolean
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
 }
-export const ImageUpload = ({values, onChange, onRemove, loading} : ImageUploadProps) => {
-    const CldUploadPreset_billboard = process.env.NEXT_PUBLIC_CLOUDINARY_BILLBOARD_UPLOAD_PRESET;
-    
-    const [isInClient, setIsInClient] = useState(false);
 
-    useEffect(() => {
-      setIsInClient(true)
-    }, []);
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
 
-    
-    const onUpload = (result: any, widget : any) => {
-        const uploadedImgUrl = result.info.secure_url;
-        onChange(uploadedImgUrl);
-        widget.close();
-    }
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if (!isInClient) {
-        return null;
-    }
-    
-  return (
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  const CldUploadPreset_billboard = process.env.NEXT_PUBLIC_CLOUDINARY_BILLBOARD_UPLOAD_PRESET;
+  return ( 
     <div>
-        <div className="mb-4 flex items-center gap-4">
-            {values.map((imgUrl) => (
-                <div 
-                key={imgUrl}
-                className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
-                    <div 
-                    className="absolute top-2 right-2 z-10"
-                    >
-                        <Button
-                        type="button"
-                        variant={"destructive"}
-                        onClick={() => onRemove(imgUrl)}
-                        size={"sm"}
-                        >
-                            <Trash className="h-4 w-4" />
-                        </Button>
-                    </div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
+            <div className="z-10 absolute top-2 right-2">
+              <Button type="button" onClick={() => onRemove(url)} variant="destructive" size="sm">
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image
+              fill
+              className="object-cover"
+              alt="Image"
+              src={url}
+            />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset={CldUploadPreset_billboard}>
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
 
-                    <Image 
-                    fill
-                    className="object-cover"
-                    src={imgUrl}
-                    alt="billboard image"
-                    />
-                </div>
-            ))}
-        </div>
-
-        <CldUploadWidget uploadPreset={CldUploadPreset_billboard} onUpload={onUpload}>
-            {({open}) => {
-
-                const onClick = () => {
-                    // Open cloudinary upload panel
-                    open();
-                }
-
-                return(
-                    <Button 
-                    type="button" 
-                    variant={"secondary"}
-                    onClick={onClick}
-                    >
-                        <PlusCircle className="mr-2"/>
-                        <span>Upload image(s)</span>
-                    </Button>
-                )
-            }}
-        </CldUploadWidget>
-
+          return (
+            <Button 
+              type="button" 
+              disabled={disabled} 
+              variant="secondary" 
+              onClick={onClick}
+            >
+              <ImagePlus className="h-4 w-4 mr-2" />
+              Upload an Image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
     </div>
-  )
+  );
 }
-
-// this component contains 
-// 1. img upload button
-// 2. img viewer
-// 3. delete uploaded img
+ 
+export default ImageUpload;
